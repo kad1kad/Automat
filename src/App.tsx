b39201d976c126600c8f31e55eb084f82bcea7c6
kad1kad <k.aliu@me.com>
2023-08-21 16:33:32 +0100

    working step sequencer

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,52 @@ function App() {
   const [isPlaying, setIsPlaying] = useState(false);
 
   const notes = ["C4", "D4", "E4", "F4", "G4", "A4", "B4"];
-  const totalSteps = 4;
+  const totalSteps: number = 8;
+
+  // Initialize stepsArrayState with individual button objects
+  const stepsArray: Array<Array<{ name: string; selected: boolean }>> =
+    Array.from({ length: totalSteps }, () =>
+      notes.map(() => ({ name: "", selected: false }))
+    );
+  const [stepsArrayState, setStepsArrayState] = useState(stepsArray);
+
+  function addNoteToSteps(
+    note: string,
+    stepIndex: number,
+    buttonIndex: number
+  ) {
+    if (note) {
+      const updatedStepsArray = [...stepsArrayState];
+      updatedStepsArray[stepIndex][buttonIndex] = {
+        name: note,
+        selected: !stepsArrayState[stepIndex][buttonIndex].selected,
+      };
+      setStepsArrayState(updatedStepsArray);
+      console.log(stepsArrayState);
+    }
+  }
 
   return (
-    <div className="px-5">
-      <h1>Synthica</h1>
+    <div className="px-5 w-screen h-screen">
+      <h1 className="text-xl py-2">Automat</h1>
       <button onClick={() => setIsPlaying(!isPlaying)}>
         {isPlaying ? "Stop" : "Play"}
       </button>
 
       {/* Step Grid */}
-      <div className="flex flex-row gap-1">
+      <div className="flex flex-row gap-1 h-[90vh]">
         {Array.from({ length: totalSteps }, (_, stepIndex) => (
-          <div className="flex flex-col gap-1" key={stepIndex}>
-            {notes.map((note) => (
-              <button className="bg-slate-500 h-12 w-12 rounded-md" key={note}>
+          <div className="flex flex-col gap-1 flex-1" key={stepIndex}>
+            {notes.map((note, buttonIndex) => (
+              <button
+                className={`flex-1 rounded-md text-slate-100 ${
+                  stepsArrayState[stepIndex][buttonIndex]?.selected
+                    ? "selected-button"
+                    : "unselected-button"
+                }`}
+                key={note}
+                onClick={() => addNoteToSteps(note, stepIndex, buttonIndex)}
+              >
                 {note}
               </button>
             ))}
@@ -31,13 +62,7 @@ function App() {
       {/* Reactronica */}
 
       <Song isPlaying={isPlaying} bpm={90}>
-        <Track
-          mute={false}
-          steps={[
-            ["C3", "E3", "G3"],
-            ["F4", "A4", "D3"],
-          ]}
-        >
+        <Track mute={false} steps={stepsArrayState}>
           <Instrument type="synth" />
         </Track>
       </Song>
