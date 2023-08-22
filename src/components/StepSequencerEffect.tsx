8ec1edca5424eefe116e788b3c55f3ee69fa9525
kad1kad <k.aliu@me.com>
2023-08-22 13:05:27 +0100

    Setup effects

---
+++ src/components/StepSequencerEffect.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+type Props = {
+  fxWet: number;
+  setFxWet: React.Dispatch<React.SetStateAction<number>>;
+};
+
+function Effect({ fxWet, setFxWet }: Props) {
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const value = parseFloat(event.target.value);
+    setFxWet(value);
+  }
+
+  return (
+    <div className="flex gap-4">
+      <div className="flex items-center gap-1">
+        <p>Delay</p>
+        <input
+          type="range"
+          value={fxWet}
+          min={0}
+          max={1}
+          step={0.01}
+          onChange={handleChange}
+          className="w-12 h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Effect;
---
+++ src/utils/effects.ts
@@ -0,0 +1,4 @@
+export const effects = [
+  { name: "Delay", type: "delay", minValue: 0, maxValue: 1 },
+  { name: "Auto Wah", type: "autoWah", minValue: 0, maxValue: 1 },
+];
