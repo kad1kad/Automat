1a9ab18e82aa0f97f925051a3523b7fa49e9bfa5
kad1kad <k.aliu@me.com>
2023-08-23 16:19:00 +0100

    add synths

--- src/utils/instruments.ts
+++ src/utils/instruments.ts
@@ -1,4 +1,6 @@
 export const instruments = [
-  { name: "Synth", type: "synth" },
+  { name: "Poly Synth", type: "polySynth" },
   { name: "AM Synth", type: "amSynth" },
+  { name: "FM Synth", type: "fmSynth" },
+  { name: "Duo Synth", type: "duoSynth" },
 ];
