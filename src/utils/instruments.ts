a79e713ec7acd66e00290296c64ea27484abe511
kad1kad <k.aliu@me.com>
2023-08-26 11:24:04 +0100

    change order

--- src/utils/instruments.ts
+++ src/utils/instruments.ts
@@ -3,6 +3,6 @@ import { InstrumentType } from "reactronica";
 export const instruments: { name: string; type: InstrumentType }[] = [
   { name: "Synth", type: "synth" },
   { name: "AM Synth", type: "amSynth" },
-  { name: "FM Synth", type: "fmSynth" },
   { name: "Duo Synth", type: "duoSynth" },
+  { name: "FM Synth", type: "fmSynth" },
 ];
